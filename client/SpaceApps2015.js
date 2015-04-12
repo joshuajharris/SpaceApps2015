27a61864689e9bd5e1fd919ec57c8b5b77e93bd8
Joshua Harris <joshua.jharris13@gmail.com>
2015-04-11 20:56:21 -0400

    Fixed error in resultLink, added title to results page, added public/data.xlsx for possible feature.

--- client/SpaceApps2015.js
+++ client/SpaceApps2015.js
@@ -1,7 +1,8 @@
 //Meteor.subscribe("data");
 
-var urls = ["https://data.oregon.gov/api/views/7zxm-9fbf/rows.json?accessType=DOWNLOAD",
-            "https://data.oregon.gov/api/views/kiyy-dbi3/rows.json?accessType=DOWNLOAD"];
+var urls = [{"url": "https://data.oregon.gov/api/views/7zxm-9fbf/rows.json?accessType=DOWNLOAD", "title": "MARINE BOARD PERMITTED EVENTS"}, 
+            {"url": "https://data.oregon.gov/api/views/kiyy-dbi3/rows.json?accessType=DOWNLOAD", "title": "Bills Signed By Governor Brown 2015"}];
+
 Session.set("urls", urls);
 
 Template.resultsList.helpers({
@@ -14,13 +15,16 @@ Template.resultsList.helpers({
 });
 
 Template.resultLink.events({
-  "click #url": function(event){
-    Meteor.call("getRes", event.currentTarget.text, function(err, res){
+  "click #title": function(event){
+    console.log(event.target.parentElement.parentElement.children.url.textContent);
+    Meteor.call("getRes", event.target.parentElement.parentElement.children.url.textContent, function(err, res){
       if(err) {
         console.error(err);
       } else {
         Session.set("cols", res.data.meta.view.columns);
         Session.set("data", res.data.data);
+        Session.set("title", res.data.meta.view.name);
+        Session.set("tags", res.data.meta.view.tags);
         console.log("done");
       }
     });
